2b753549e183c14a4de705c6ca99ffcd44a07dae
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-08 23:55:29 -0300

    chore: remove App.css and integrate Tailwind CSS in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 // @ts-ignore
@@ -6,5 +7,5 @@ import jsconfigPaths from 'vite-jsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
 	// @ts-ignore
-	plugins: [react(), jsconfigPaths()],
+	plugins: [react(), jsconfigPaths(), tailwindcss()],
 });
