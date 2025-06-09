eb261b16122eb080977a4afd89c14566c5a64ace
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-09 19:52:01 -0300

    chore: update main.jsx to import global styles and load custom fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import './index.css';
+import './styles/global.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.jsx';
+import { loadFonts } from './utils/loadFonts.js';
 
 const ROOT = document.getElementById('root');
+
+loadFonts(['1em Satoshi', '700 1em IntegralCF']);
 
 if (ROOT) {
 	createRoot(ROOT).render(
