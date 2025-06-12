54978afaf38db4f56c0d962c5ed2a6852cb0929f
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-12 03:58:20 -0300

    feat: replace App component with AppRouter and wrap in ErrorBoundary for improved error handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-import './styles/global.css';
+import '@styles/global.css';
 
+import ErrorBoundary from '@components/ErrorBoundary.jsx';
+import { loadFonts } from '@utils/loadFonts.js';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.jsx';
-import { loadFonts } from './utils/loadFonts.js';
-
-const ROOT = document.getElementById('root');
+import AppRouter from './AppRouter.jsx';
 
 loadFonts(['1em Satoshi', '700 1em IntegralCF']);
+
+const ROOT = document.getElementById('root');
 
 if (ROOT) {
 	createRoot(ROOT).render(
 		<StrictMode>
-			<App />
+			<ErrorBoundary>
+				<AppRouter />
+			</ErrorBoundary>
 		</StrictMode>,
 	);
 }
