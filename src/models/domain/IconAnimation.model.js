2dad8f2b36890a1e84df2f9a443c03d1c109ce0c
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-12 03:46:55 -0300

    feat: add IconAnimation model for menu animation control

---
+++ src/models/domain/IconAnimation.model.js
@@ -0,0 +1,7 @@
+/**
+ * @typedef {Object} IconAnimation
+ * @property {() => void} startAnimation - Start the menu animation
+ * @property {() => void} stopAnimation - Stop the menu animation
+ */
+
+export {};
