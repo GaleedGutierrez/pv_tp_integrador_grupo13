624fb6f9fcdfcb40d9045865fab648317513543f
vasquez franklin <franklinvasquez206@gmail.com>
2025-06-17 20:28:30 -0300

    feat: agregue el models.js

---
+++ src/models/searchVideo.model.js
@@ -0,0 +1,19 @@
+// import { YoutubeValidator } from '@utils/YoutubeValidator';
+// import { z } from 'zod';
+// export const searchVideoSchema = z.object({
+// 	youtubeUrl: z
+// 		.string()
+// 		.transform((value) => value.trim())
+// 		.refine(
+// 			(url) => {
+// 				if (url === '') {
+// 					return true;
+// 				}
+// 				return YoutubeValidator.validateUrl(url);
+// 			},
+// 			{
+// 				message: 'Verifica la URL',
+// 			},
+// 		),
+// });
+// export type SearchVideoData = z.infer<typeof searchVideoSchema>;
