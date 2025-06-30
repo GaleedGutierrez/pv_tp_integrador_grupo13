2b92cb1d4b5a1591b7da93d3e834f9a2236396b1
Alfredo Ezequiel Gonzalez Lopez <eg156765@gmail.com>
2025-06-30 19:12:39 -0300

    refactor: clean up comments and remove unnecessary imports in favorites actions and slice

--- src/sections/favorites/hooks/useFavoritesActions.js
+++ src/sections/favorites/hooks/useFavoritesActions.js
@@ -1,19 +1,18 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
 import { useAppDispatch } from '@hooks/useAppDispatch';
 
 import {
 	addToFavorite as addFavorite,
 	deleteFavoriteById as deleteFavorite,
-} from '../slice/favoritesSlice.js';
-
-/** @import { Product } from '@/modules/products/domain/ProductRepository.js' */
+} from '../slice/favoritesSlice';
 
 export const useFavoritesActions = () => {
 	const dispatch = useAppDispatch();
-	/** @param {Product} product - Product to add favorite */
 	const addToFavorite = (product) => {
 		dispatch(addFavorite(product));
 	};
-	/** @param {string} id - ID of product to delete from favorites */
 	const deleteFavoriteById = (id) => {
 		dispatch(deleteFavorite(id));
 	};
