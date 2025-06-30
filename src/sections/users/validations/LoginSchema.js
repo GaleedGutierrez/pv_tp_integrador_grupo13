652158db07676c8848ef084e848d47f61a7acb59
Alfredo Ezequiel Gonzalez Lopez <eg156765@gmail.com>
2025-06-30 19:19:32 -0300

    feat: add validation schemas for Login and Register forms, and prepare UserProfile schema for future implementation

---
+++ src/sections/users/validations/LoginSchema.js
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+import * as z from 'zod';
+
+export const LoginSchema = z.object({
+	email: z
+		.string({ required_error: 'El correo electrónico es obligatorio.' })
+		.nonempty({ message: 'El correo electrónico no puede estar vacío.' })
+		.email({
+			message: 'Verifica el correo electrónico ingresado.',
+		}),
+	password: z
+		.string({ required_error: 'La contraseña es obligatoria.' })
+		.nonempty({
+			message: 'La contraseña no puede estar vacía.',
+		}),
+});
---
+++ src/sections/users/validations/RegisterSchema.js
@@ -0,0 +1,28 @@
+import * as z from 'zod';
+
+export const RegisterSchema = z
+	.object({
+		name: z.string().optional(),
+		lastname: z.string().optional(),
+		email: z.string().email({
+			message: 'Verifica el correo electrónico ingresado.',
+		}),
+		username: z.string().optional(),
+		password: z.string().min(6, {
+			message: 'La contraseña debe tener al menos 6 caracteres.',
+		}),
+		confirmPassword: z.string(),
+	})
+	.refine(
+		(data) => {
+			if (!data.password || !data.confirmPassword) {
+				return true;
+			}
+
+			return data.password === data.confirmPassword;
+		},
+		{
+			message: 'Las contraseñas no coinciden.',
+			path: ['confirmPassword'],
+		},
+	);
