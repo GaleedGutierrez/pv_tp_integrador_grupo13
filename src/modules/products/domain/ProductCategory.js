3921f99478dabd1142d6b1c769e4cf750411ad94
vasquez franklin <franklinvasquez206@gmail.com>
2025-06-30 18:57:44 -0300

    refactor: remove unused type definitions and comments from Product files

--- src/modules/products/domain/ProductCategory.js
+++ src/modules/products/domain/ProductCategory.js
@@ -1,13 +1,6 @@
-/**
- * @enum {string}
- */
 export const ProductCategory = {
 	Electronics: 'electronics',
 	Jewelry: 'jewelery',
 	MensClothing: "men's clothing",
 	WomensClothing: "women's clothing",
 };
-
-/**
- * @typedef {typeof ProductCategory[keyof typeof ProductCategory]} TypeProductCategory
- */
