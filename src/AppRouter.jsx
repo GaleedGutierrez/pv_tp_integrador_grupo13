389f5df5c0268e637100dcaf0e362a3fd7e5a12d
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-30 19:30:10 -0300

    feat: refactor AppRouter to implement authentication-based routing with loading state

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,11 +1,8 @@
-import { appRoutes } from '@routes/appRouters';
-import { Home } from '@views/Home/index';
-import { Layout } from '@views/Layout';
-import { BrowserRouter, Route } from 'react-router';
-
-import { Favorites } from './views/Favorites';
-import { RoutesWithNotFound } from './views/NotFoundPage';
-import { ProductsDetails } from './views/ProductsDetails';
+import { useAuthContext } from '@context/auth.context';
+import { PrivateApp } from '@views/private/PrivateApp';
+import { PublicApp } from '@views/public/PublicApp';
+import { LoaderIcon } from 'lucide-react';
+import { BrowserRouter } from 'react-router';
 
 /**
  * Main application router.
@@ -13,35 +10,24 @@ import { ProductsDetails } from './views/ProductsDetails';
  * It defines the routes and their corresponding components.
  */
 function AppRouter() {
+	const { auth } = useAuthContext();
+	const { isLoggedIn, isLoading } = auth;
+
+	if (isLoading) {
+		return (
+			<section className="my-8 flex flex-col items-center justify-center gap-4">
+				<h1>Cargando...</h1>
+				<LoaderIcon
+					className="animate-spin"
+					size={80}
+				/>
+			</section>
+		);
+	}
+
 	return (
 		<BrowserRouter>
-			<RoutesWithNotFound>
-				<Route
-					element={<Layout />}
-					path={appRoutes.home.index}
-				>
-					<Route
-						element={<Home />}
-						path={appRoutes.home.index}
-					/>
-					<Route
-						element={<Favorites />}
-						path={appRoutes.favorites.list}
-					/>
-					<Route
-						element={<ProductsDetails />}
-						path={appRoutes.products.routes.details}
-					/>
-					{/* <Route
-            element={<CreateProduct />}
-            path={appRoutes.products.create}
-        /> */}
-					{/* <Route
-        element={<UpdateProduct />}
-        path={AppRoutes.products.updateProduct}
-    /> */}
-				</Route>
-			</RoutesWithNotFound>
+			{isLoggedIn ? <PrivateApp /> : <PublicApp />}
 		</BrowserRouter>
 	);
 }
